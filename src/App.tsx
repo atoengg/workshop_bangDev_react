5fea5fa70af28bf8bb5df0b9b39cf5e827cb48a8
atoenggDev <kromosom06@gmail.com>
2024-03-30 16:31:46 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-teal-500 flex justify-center items-center text-xl">
+        Berhasil Installasi vite + TSX + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
