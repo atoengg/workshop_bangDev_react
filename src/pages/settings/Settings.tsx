4519d9a2f8c2ce99f73f7eab83fe54f95374aed3
atoenggDev <kromosom06@gmail.com>
2024-03-31 15:20:13 +0700

    modify: folder in pages

--- src/pages/settings/Settings.tsx
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-
-const Settings = () => {
-  return <div>ini halaman Settings</div>;
-};
-
-export default Settings;
--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
-import Settings from "../pages/settings/Settings";
 
 const Routers = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/settings" element={<Settings />} />
       </Routes>
     </>
   );
